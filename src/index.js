7126b21390f1236f27d1bbdc332a075c0c3cd02c
Vishnudewangan <dewanganvish1721@gmail.com>
2022-07-07 21:40:20 +0530

    dispatch Actions

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import { createStore } from 'redux';// createStore deprecated thats I am using configureStore instead
-import { configureStore } from '@reduxjs/toolkit'; // in place of createStore
+import { createStore } from 'redux';// createStore deprecated thats I am using configureStore instead
+//import { configureStore } from '@reduxjs/toolkit'; // in place of createStore
 
 import './index.css';
 import App from './components/App';
 import reducer from './reducers';
 import movies from './reducers';
 
-;
-const store = configureStore(movies); // reducer is the function that returns the state // movies is the initial state // createStore is a function that takes a reducer and returns a store// createStore deprecated in favor of configureStore
-console.log('store', store);
-console.log('STATE', store.getState());
 
+//const store = configureStore(movies); // reducer is the function that returns the state // movies is the initial state // createStore is a function that takes a reducer and returns a store// createStore deprecated in favor of configureStore
+const store = createStore(reducer); // reducer is the function that returns the state // movies is the initial state // createStore is a function that takes a reducer and returns a store
+console.log('store', store);
+console.log('BEFORE STATE', store.getState());
+
+store.dispatch({
+    type: 'ADD_MOVIES',
+     movies: [{ name: 'Superman'}]
+});
+
+console.log('AFTER STATE', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
     <App />
- 
 );
